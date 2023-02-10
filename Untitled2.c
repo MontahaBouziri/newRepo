481d82552396d8618e19b8b2ebd1f83c651df92f
MontahaBouziri <montaha.bouziri@gmail.com>
2023-02-10 12:31:33 +0100

    modifying

--- Untitled2.c
+++ Untitled2.c
@@ -3,7 +3,8 @@
 
 int main(){
 	int x=0,y=0;
-	printf("donner x et y");
+	printf("donner x et y\n");
 	scanf(" %d%d ",&x,&y);
-	printf(" %d +  %d =  %d",x,y,x+y);
+	printf(" %d +  %d =  %d\n",x,y,x+y);
+	printf("changes have been made");
 }
